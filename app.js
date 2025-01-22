67bd1e152e49a9ef22eb59237eb81b6daadd0f8e
felipe domingues barbosa <felipe.lp2904@gmail.com>
2025-01-22 13:21:01 -0300

    fim da aula 2

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 alert ("Bem-vindo ao melhor jogo do mundo");
-let numeroSecreto = 9;
+numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 );
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto
-while (chute != numeroSecreto){
-    chute = prompt("Escolha um numero entre 1 e 10");
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um numero entre 1 e ${numeroMaximo}`);
     // se o numero for igual o numero secreto
-    if (chute == numeroSecreto){
+    if (chute == numeroSecreto) {
         break;
     } else{
-        if (chute > numeroSecreto){
+        if (chute > numeroSecreto) {
             alert (`O numero secreto e menor que ${chute}`);
         } else {
             alert (`o numero secreto e maior que ${chute}`);
@@ -21,5 +22,5 @@ while (chute != numeroSecreto){
      }
 }
 
-let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
-alert (`Parabens voce acertou o numero secreto ${numeroSecreto} com ${Tentativas} ${palavraTentativas}.`);
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert (`Parabens voce acertou o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
